ef82847bd2ac366e59b12bd753c2e9b390bf15f2
Felipe Lagos <felagossanchez@gmail.com>
2025-08-04 19:49:28 -0400

    Refactor main.py: Enhance type definitions for BookInfo and BookData, and update process_category_page to return BookData

--- src/scraping_py/main.py
+++ src/scraping_py/main.py
@@ -1,16 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import TypedDict, Dict
 
 domain: str = "https://books.toscrape.com/"
 url_seed: str = "https://books.toscrape.com/"
+urls_processed: Dict[str, bool] = {}
+
+class BookInfo(TypedDict):
+    url: str
+    title: str
+    in_stock: bool
+    price: float
+
+class BookData(TypedDict):
+    url: str
+    info: BookInfo
 
 def get_html_parser(url: str) -> BeautifulSoup:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     return soup
 
-def process_category_page(url: str) -> None:
-    pass
+def process_category_page(url: str) -> BookData:
+    books_data: BookData = {}
+
+    soup = get_html_parser(url)
+    
+    books_data
 
 def load_category_urls(soup: BeautifulSoup) -> list[str]:
     url_frontier: list[str] = []
@@ -25,9 +41,12 @@ def load_category_urls(soup: BeautifulSoup) -> list[str]:
 def main():
     soup_main_page = get_html_parser(url_seed)
 
-    url_frontier: list[str] = load_category_urls(soup_main_page)
+    url_categories_frontier: list[str] = load_category_urls(soup_main_page)
 
-
+    for category_url in url_categories_frontier:
+        if category_url not in urls_processed:
+            urls_processed[category_url] = True
+            process_category_page(category_url)
 
 if __name__ == "__main__":
     main()
